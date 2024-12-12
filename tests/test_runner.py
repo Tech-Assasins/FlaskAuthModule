274ef7dbace6a51c336c1f4b01dfa1a8fe3f1896
thief01 <pst12345@wp.pl>
2024-12-12 13:58:53 +0100

    remove useless code

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,13 +1,5 @@
 from tests.account_test import AccountTest
 from tests.auth_test import AuthTest
 from tests.db_test import DbTest
-import pyodbc
-
-def run_auth_lib_tests():
-    AccountTest()
-    AuthTest()
-    DbTest()
 
 
-if __name__ == '__main__':
-    run_auth_lib_tests()
